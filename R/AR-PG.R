1bf26893e0e35b17d54489fb278d9d53ea4f5350
Kevin Middleton <middletonk@missouri.edu>
2020-07-28 16:39:42 -0500

    Complete data entry for AR traits

---
+++ R/AR-PG.R
@@ -0,0 +1,23 @@
+#' Articulare-Pogonion
+#' 
+#' Variable AR-PG from Riolo et al. (1974)
+#' 
+#' 
+#' @name AR_PG
+#' @docType data
+#' @format A data.frame with 22 observations of the following 10 variables: \describe{
+#'   \item{Variable_Num}{Variable number of Riolo}
+#'   \item{Type}{Variable type (Angular or Linear)}
+#'   \item{Long_Name}{Descriptive variable name}
+#'   \item{Short_Name}{Abbreviated variable name}
+#'   \item{Numeric_Name}{Numeric landmark coding}
+#'   \item{Age}{Age in years}
+#'   \item{Sex}{Female or Male}
+#'   \item{N}{Numeber of observations}
+#'   \item{Mean}{Mean value}
+#'   \item{SD}{Standard deviation}
+#' }
+#' @keywords datasets
+#' 
+NULL
+
---
+++ R/CO-GO.R
@@ -0,0 +1,23 @@
+#' Condylion-Gonion
+#' 
+#' Variable CO-GO from Riolo et al. (1974)
+#' 
+#' 
+#' @name CO_GO
+#' @docType data
+#' @format A data.frame with 22 observations of the following 10 variables: \describe{
+#'   \item{Variable_Num}{Variable number of Riolo}
+#'   \item{Type}{Variable type (Angular or Linear)}
+#'   \item{Long_Name}{Descriptive variable name}
+#'   \item{Short_Name}{Abbreviated variable name}
+#'   \item{Numeric_Name}{Numeric landmark coding}
+#'   \item{Age}{Age in years}
+#'   \item{Sex}{Female or Male}
+#'   \item{N}{Numeber of observations}
+#'   \item{Mean}{Mean value}
+#'   \item{SD}{Standard deviation}
+#' }
+#' @keywords datasets
+#' 
+NULL
+
