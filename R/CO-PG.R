98cc1e1502d1f56431681067a2be4d073649834d
Kevin Middleton <middletonk@missouri.edu>
2020-07-28 13:19:56 -0500

    Add CO-GN and CO_PG

---
+++ R/CO-PG.R
@@ -0,0 +1,23 @@
+#' Condylion-Pogonion
+#' 
+#' Variable CO-PG from Riolo et al. (1974)
+#' 
+#' 
+#' @name CO_PG
+#' @docType data
+#' @format A data.frame with 32 observations of the following 10 variables: \describe{
+#'   \item{Variable_Num}{Variable number of Riolo}
+#'   \item{Type}{Variable type (Angular or Linear)}
+#'   \item{Long_Name}{Descriptive variable name}
+#'   \item{Short_Name}{Abbreviated variable name}
+#'   \item{Numeric_Name}{Numeric landmark coding}
+#'   \item{Age}{Age in years}
+#'   \item{Sex}{Female or Male}
+#'   \item{N}{Numeber of observations}
+#'   \item{Mean}{Mean value}
+#'   \item{SD}{Standard deviation}
+#' }
+#' @keywords datasets
+#' 
+NULL
+
---
+++ R/Riolo.R
@@ -0,0 +1,26 @@
+#' Data sets from Riolo et al. (1974)
+#'
+#' The \code{abd} package contains data sets and sample code for the book,
+#' \emph{The Analysis of Biological Data} by Michael C. Whitlock and Dolph
+#' Schluter (2009; Roberts and Company Publishers).
+#'
+#' \tabular{ll}{ Package: \tab Riolo\cr
+#'               Type: \tab Package\cr
+#'               Version: \tab 0.1.0\cr
+#'               Date: \tab 2020-07-28\cr
+#'               License: \tab MIT\cr
+#'               LazyLoad: \tab yes\cr
+#'               LazyData: \tab yes\cr }
+#'
+#' @name Riolo-package
+#' @aliases Riolo-package Riolo
+#' @docType package
+#' @author Kevin M. Middleton (\url{middletonk@@missouri.edu})
+#' @references Riolo ML, Moyers RE, McNamara JA, Hunter WS. 1974. An atlas of
+#'   craniofacial growth: cephalometric standards from the University School
+#'   Growth Study, the University of Michigan. Craniofacial Growth Series
+#'   Monograph 2.
+#' @keywords package
+#'
+#'
+NULL
