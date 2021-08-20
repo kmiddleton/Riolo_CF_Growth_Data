b5468878972787bc83f050bad96f574d79c96b15
Kevin Middleton <middletonk@missouri.edu>
2021-08-20 07:50:03 -0500

    Update get_Riolo_data.R

--- R/get_Riolo_data.R
+++ R/get_Riolo_data.R
@@ -2,10 +2,11 @@
 #'
 #' @param Riolo_var string Riolo variable name
 #' @param sex string for sex
-#' @param rescale Boolean for whether to rescale data using Michigan scaling
-#' factor
+#' @param rescale Boolean for whether to rescale data using Michigan
+#' enlargement factor
 #'
 #' @return data.frame with Riolo data
+#'
 #' @export
 #'
 get_Riolo_data <- function(Riolo_var, sex, rescale = FALSE) {
